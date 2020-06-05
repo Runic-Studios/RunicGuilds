1c597feca0d26226a814a17a153f09540f868ab5
Skyfallin <zextertech777@yahoo.com>
2020-06-05 12:32:00 -0700

    bosses now spawn at correct time

--- src/main/java/com/runicrealms/runicguilds/boss/GuildBossManager.java
+++ src/main/java/com/runicrealms/runicguilds/boss/GuildBossManager.java
@@ -8,16 +8,25 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.scheduler.BukkitRunnable;
 
+@SuppressWarnings("deprecation")
 public class GuildBossManager {
 
-    private static final int SPAWN_TIMER = 1; // minutes (90?)
+    private static final int SPAWN_TIMER = 90; // minutes (90?)
     private static final int KILL_POINTS = 20;
 
     public GuildBossManager() {
 
         // 30 min before
+        Bukkit.getScheduler().scheduleAsyncDelayedTask(Plugin.getInstance(),
+                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "A guild boss will spawn in 30 minutes!"), (SPAWN_TIMER-30)*60*20L);
+
         // 15 min before
+        Bukkit.getScheduler().scheduleAsyncDelayedTask(Plugin.getInstance(),
+                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "A guild boss will spawn in 15 minutes!"), (SPAWN_TIMER-15)*60*20L);
+
         // 5 min before
+        Bukkit.getScheduler().scheduleAsyncDelayedTask(Plugin.getInstance(),
+                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "A guild boss will spawn in 5 minutes!"), (SPAWN_TIMER-5)*60*20L);
 
         new BukkitRunnable() {
             @Override
