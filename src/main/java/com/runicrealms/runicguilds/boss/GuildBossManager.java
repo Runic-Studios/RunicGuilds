a3110b22927a008d952e15fb3e9972170f075a78
Skyfallin <dimetrius.hightower@gmail.com>
2020-07-24 15:56:06 -0700

    fixed guild prefix formatting

--- src/main/java/com/runicrealms/runicguilds/boss/GuildBossManager.java
+++ src/main/java/com/runicrealms/runicguilds/boss/GuildBossManager.java
@@ -19,15 +19,15 @@ public class GuildBossManager {
 
         // 30 min before
         Bukkit.getScheduler().scheduleAsyncDelayedTask(Plugin.getInstance(),
-                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + PREFIX + "A guild boss will spawn in 30 minutes!"), (SPAWN_TIMER-30)*60*20L);
+                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + PREFIX + " A guild boss will spawn in 30 minutes!"), (SPAWN_TIMER-30)*60*20L);
 
         // 15 min before
         Bukkit.getScheduler().scheduleAsyncDelayedTask(Plugin.getInstance(),
-                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + PREFIX + "A guild boss will spawn in 15 minutes!"), (SPAWN_TIMER-15)*60*20L);
+                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + PREFIX + " A guild boss will spawn in 15 minutes!"), (SPAWN_TIMER-15)*60*20L);
 
         // 5 min before
         Bukkit.getScheduler().scheduleAsyncDelayedTask(Plugin.getInstance(),
-                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + PREFIX + "A guild boss will spawn in 5 minutes!"), (SPAWN_TIMER-5)*60*20L);
+                () -> Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + PREFIX + " A guild boss will spawn in 5 minutes!"), (SPAWN_TIMER-5)*60*20L);
 
         new BukkitRunnable() {
             @Override
