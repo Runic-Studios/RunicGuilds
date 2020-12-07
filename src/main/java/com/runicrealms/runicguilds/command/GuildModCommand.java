b7db2294f4df8c5e04c9a631ebdba3a3c5c6cf2a
BoBoBalloon <61363161+BoBoBalloon@users.noreply.github.com>
2020-12-07 18:29:38 -0500

    sorry for the spam, cleaned up sub-command body

--- src/main/java/com/runicrealms/runicguilds/command/GuildModCommand.java
+++ src/main/java/com/runicrealms/runicguilds/command/GuildModCommand.java
@@ -3,24 +3,14 @@ package com.runicrealms.runicguilds.command;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import com.runicrealms.plugin.utilities.ColorUtil;
-import com.runicrealms.runicguilds.Plugin;
 import com.runicrealms.runicguilds.api.GiveGuildEXPEvent;
 import com.runicrealms.runicguilds.data.GuildUtil;
-import com.runicrealms.runicguilds.gui.GuildBannerUI;
 import com.runicrealms.runicguilds.guilds.EXPSource;
+import com.runicrealms.runicguilds.guilds.ForceLoadBanners;
 import com.runicrealms.runicguilds.guilds.Guild;
-import com.runicrealms.runicguilds.guilds.PostedGuildBanner;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 public class GuildModCommand extends BaseCommand {
 
@@ -144,32 +134,6 @@ public class GuildModCommand extends BaseCommand {
     @Subcommand("forceloadbanners")
     @CommandPermission("runicadmin.guilds.forceloadbanners")
     public void onGuildModGiveEXPCommand(CommandSender sender) { //made it CommandSender because it might be console (fix if wrong please)
-        for (PostedGuildBanner banner : Plugin.getPostedGuildBanners()) {
-            banner.remove();
-        }
-
-        List<Guild> ordering = new ArrayList<>(GuildUtil.getAllGuilds());
-        List<Guild> guilds = new ArrayList<>();
-        Comparator<Guild> comparator = Comparator.comparing(Guild::getScore).reversed();
-
-        Collections.sort(ordering, comparator);
-
-        for (int i = 0; i < 3; i++) {
-            guilds.add(ordering.get(i));
-        }
-
-        Integer i = 1;
-        for (Guild guild : guilds) {
-            String path = "banners.number" + i.toString();
-            FileConfiguration config = Plugin.getInstance().getConfig();
-            World world = Bukkit.getWorld(config.getString(path + ".world"));
-            double x = config.getDouble(path + ".x");
-            double y = config.getDouble(path + ".y");
-            double z = config.getDouble(path + ".z");
-            Location location = new Location(world, x, y, z);
-            PostedGuildBanner banner = new PostedGuildBanner(guild, location);
-            Plugin.getPostedGuildBanners().add(banner);
-            i++;
-        }
+        new ForceLoadBanners().run();
     }
 }
