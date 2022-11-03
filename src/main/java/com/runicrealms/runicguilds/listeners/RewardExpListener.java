2e49208b41a5470986736d2c000290e7c881be2e
Skyfallin <skyfallin.business@gmail.com>
2022-11-03 14:09:41 -0400

    RR-111 damage buff works

--- src/main/java/com/runicrealms/runicguilds/listeners/RewardExpListener.java
+++ src/main/java/com/runicrealms/runicguilds/listeners/RewardExpListener.java
@@ -6,6 +6,7 @@ import com.runicrealms.runicguilds.guild.stage.GuildStage;
 import com.runicrealms.runicguilds.guild.stage.StageReward;
 import com.runicrealms.runicguilds.model.GuildData;
 import com.runicrealms.runicguilds.util.GuildUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -20,15 +21,24 @@ public class RewardExpListener implements Listener {
     public void onPlayerGainExperience(RunicExpEvent event) {
 
         Player player = event.getPlayer();
+
+        // ensure this perk can be unlocked
+        StageReward stageReward = StageReward.EXP_BONUS;
+        GuildStage guildStage = GuildStage.getFromReward(stageReward);
+        if (guildStage == null) return;
+
+        // ensure there is a guild
         GuildData guildData = GuildUtil.getGuildData(player.getUniqueId());
         if (guildData == null) return;
+
+        // ensure guild has perk unlocked
         Guild guild = guildData.getData();
-        if (guild.getGuildExp() < GuildStage.getMaxStage().getExp()) return;
+        if (guild.getGuildStage().getRank() < guildStage.getRank()) return;
 
         int eventExperience = event.getOriginalAmount(); // exp before other bonuses so we don't apply compound bonuses
-        StageReward stageReward = StageReward.COMBAT_BONUS;
         eventExperience *= stageReward.getBuffPercent(); // determine bonus amount
 
+        Bukkit.broadcastMessage("old was: " + event.getFinalAmount() + " and new is " + (event.getFinalAmount() + eventExperience));
         event.setFinalAmount(event.getFinalAmount() + eventExperience);
     }
 
