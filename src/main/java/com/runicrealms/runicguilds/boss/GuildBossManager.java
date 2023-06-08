07c65f4e3bd8144b0112ef922872dd62055ceab5
Excel619 <excel8392@gmail.com>
2023-06-08 11:58:42 -0600

    Fixed bugs with guildboss command

--- src/main/java/com/runicrealms/runicguilds/boss/GuildBossManager.java
+++ src/main/java/com/runicrealms/runicguilds/boss/GuildBossManager.java
@@ -4,7 +4,6 @@ import com.runicrealms.plugin.common.RunicCommon;
 import com.runicrealms.plugin.common.util.Pair;
 import com.runicrealms.plugin.events.MagicDamageEvent;
 import com.runicrealms.plugin.events.PhysicalDamageEvent;
-import com.runicrealms.plugin.events.RunicDamageEvent;
 import com.runicrealms.runicguilds.RunicGuilds;
 import com.runicrealms.runicguilds.model.GuildInfo;
 import com.runicrealms.runicguilds.model.MemberData;
@@ -18,6 +17,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -26,7 +26,7 @@ import java.util.UUID;
 public class GuildBossManager implements Listener {
 
     private final Map<UUID, Pair<Map<UUID, Integer>, Double>> bossDamage = new HashMap<>(); // first uuid is boss entity ID, map is of player -> damage, last integer is boss max health
-    private Set<String> bosses;
+    private final Set<String> bosses = new HashSet<>();
 
     public GuildBossManager() {
         Bukkit.getPluginManager().registerEvents(this, RunicGuilds.getInstance());
@@ -38,30 +38,34 @@ public class GuildBossManager implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGH)
-    public void onDamageBoss(RunicDamageEvent event) {
+    public void onPhysicalDamageBoss(PhysicalDamageEvent event) {
         if (event.isCancelled()) return;
-        Player player = null;
-        if (event instanceof MagicDamageEvent magicDamageEvent) {
-            player = magicDamageEvent.getPlayer();
-        } else if (event instanceof PhysicalDamageEvent physicalDamageEvent) {
-            player = physicalDamageEvent.getPlayer();
-        }
-        if (player == null) return;
-        if (bossDamage.containsKey(event.getVictim().getUniqueId())) {
-            Map<UUID, Integer> damage = bossDamage.get(event.getVictim().getUniqueId()).first;
-            if (!damage.containsKey(player.getUniqueId())) {
-                damage.put(player.getUniqueId(), 0);
+        handleBossDamage(event.getPlayer(), event.getVictim().getUniqueId(), event.getAmount());
+    }
+
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onMagicDamageBoss(MagicDamageEvent event) {
+        if (event.isCancelled()) return;
+        handleBossDamage(event.getPlayer(), event.getVictim().getUniqueId(), event.getAmount());
+    }
+
+    public void handleBossDamage(Player damager, UUID bossID, int damageAmount) {
+        if (bossDamage.containsKey(bossID)) {
+            Map<UUID, Integer> damage = bossDamage.get(bossID).first;
+            if (!damage.containsKey(damager.getUniqueId())) {
+                damage.put(damager.getUniqueId(), 0);
             }
-            damage.put(player.getUniqueId(), damage.get(player.getUniqueId()) + event.getAmount());
+            damage.put(damager.getUniqueId(), damage.get(damager.getUniqueId()) + damageAmount);
         } else {
-            if (!MythicMobs.inst().getMobManager().isActiveMob(event.getVictim().getUniqueId())) return;
-            Optional<ActiveMob> optMob = MythicMobs.inst().getMobManager().getActiveMob(event.getVictim().getUniqueId());
+            Optional<ActiveMob> optMob = MythicMobs.inst().getMobManager().getActiveMob(bossID);
             if (optMob.isPresent()) {
                 ActiveMob activeMob = optMob.get();
+                if (!bosses.contains(activeMob.getMobType())) return;
                 Map<UUID, Integer> damage = new HashMap<>();
-                damage.put(player.getUniqueId(), event.getAmount());
-
-                bossDamage.put(event.getVictim().getUniqueId(), new Pair<>(damage, activeMob.getEntity().getMaxHealth()));
+                damage.put(damager.getUniqueId(), damageAmount);
+                double maxHealth = activeMob.getEntity().getMaxHealth();
+                bossDamage.put(bossID, new Pair<>(damage, maxHealth));
             }
         }
     }
@@ -74,23 +78,26 @@ public class GuildBossManager implements Listener {
         // 75% of guild score is distributed according to how much damage each player did, 25% is split evenly for participation
         int damageScoreTotal = (int) Math.round(guildScore * 0.75);
         Map<UUID, Integer> damageScores = new HashMap<>();
+        double participationScoreTotal = guildScore * 0.25;
+        double participationScoreForEach = participationScoreTotal / ((double) damageValues.size());
+
         for (UUID damager : damageValues.keySet()) {
             double percentDamage = damageValues.get(damager) / maxHealth;
-            damageScores.put(damager, (int) Math.round(damageScoreTotal * percentDamage));
-        }
-        int participationScoreTotal = (int) Math.round(guildScore * 0.25);
-        int participationScoreForEach = (int) Math.round(participationScoreTotal / ((double) damageValues.size()));
-        for (UUID damager : damageValues.keySet()) {
-            damageScores.put(damager, damageScores.get(damager) + participationScoreForEach);
+            damageScores.put(damager, (int) Math.round(damageScoreTotal * percentDamage + participationScoreForEach));
         }
         // Distribute guild score
         Bukkit.getScheduler().runTaskAsynchronously(RunicGuilds.getInstance(), () -> {
             for (UUID damager : damageScores.keySet()) {
                 GuildInfo info = RunicGuilds.getDataAPI().getGuildInfo(Bukkit.getOfflinePlayer(damager));
+                if (info == null) continue;
                 MemberData data = RunicGuilds.getDataAPI().loadMemberData(info.getGuildUUID(), damager);
-                RunicGuilds.getGuildsAPI().addGuildScore(info.getGuildUUID(), data, damageScores.get(damager));
+                Bukkit.getScheduler().runTask(RunicGuilds.getInstance(), () -> {
+                    RunicGuilds.getGuildsAPI().addGuildScore(info.getGuildUUID(), data, damageScores.get(damager));
+                    // TODO send "player gained guild score" to all online players
+                });
             }
         });
+        bossDamage.remove(bossID);
     }
 
 }
