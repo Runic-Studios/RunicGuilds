51c59b7705f0c782d84699a40527eaf174691934
Excel619 <excel8392@gmail.com>
2023-06-27 23:07:33 -0400

    Removed wildcard imports

--- src/main/java/com/runicrealms/runicguilds/command/GuildCommandMapManager.java
+++ src/main/java/com/runicrealms/runicguilds/command/GuildCommandMapManager.java
@@ -1,6 +1,10 @@
 package com.runicrealms.runicguilds.command;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Some guild commands map to multiple functions (e.g. accept, confirm)
